2b8c90f001f61b925ca9941ab02d9170a0a93b1d
Besthost <besthostgambler@gmail.com>
2022-05-21 23:45:11 +0200

    increased pokemon limit

--- hooks/useFetchPokemons.tsx
+++ hooks/useFetchPokemons.tsx
@@ -1,7 +1,7 @@
 import type {PokemonUrl} from '../pages/index'
 
 export async function fetchPokemonUrls() {
-  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=200');
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=600');
   return res.json();
 }
 
